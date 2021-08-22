691f3ea15210ff0c38d78dca0e5f37bdb9fb58de
archdim1 <archibelousov@gmail.com>
2021-08-22 12:44:50 +0300

    First version of application moved into github

--- crm/forms.py
+++ crm/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from django.core.exceptions import ValidationError
-
 from .models import Company, Phone, Email, Manager, Project, Interaction, ManagerCRM, User, Customer
 from ckeditor.widgets import CKEditorWidget
 from django.forms.models import inlineformset_factory
 
 
 class CompanyModelForm(forms.ModelForm):
-
+    """
+    Класс по созданию формы для модели Company.
+    """
     class Meta:
+        """
+        Отобраает поля из модели в форме.
+        """
         model = Company
         fields = '__all__'
 
     def clean_title(self):
+        """
+        Проверяет валидность названия компании.
+        """
         title = self.cleaned_data.get('title')
 
         qs = Company.objects.filter(title__iexact=title)
@@ -37,13 +44,22 @@ ManagerFormSet = inlineformset_factory(Company, Manager,
 
 
 class ProjectModelForm(forms.ModelForm):
+    """
+    Класс по созданию формы для модели Project.
+    """
 
     class Meta:
+        """
+        Отобраает поля из модели в форме.
+        """
         model = Project
         fields = ['company', 'name', 'description', 'start_date', 'end_date', 'price']
 
 
     def clean_title(self):
+        """
+        Проверяет валидность названия проекта.
+        """
         name = self.cleaned_data.get('name')
 
         qs = Project.objects.filter(title__iexact=name)
@@ -53,25 +69,29 @@ class ProjectModelForm(forms.ModelForm):
             raise forms.ValidationError('Введите уникальное название')
         return name
 
-    # def clean(self):
-    #     start_date = self.cleaned_data['start_date']
-    #     end_date = self.cleaned_data['end_date']
-    #
-    #     if end_date <= start_date:
-    #         raise forms.ValidationError("End date must be later than start date")
-    #     return super(ProjectModelForm, self).clean()
-
 
 class InteractionModelForm(forms.ModelForm):
+    """
+    Класс по созданию формы для модели Interaction.
+    """
 
     class Meta:
+        """
+        Отобраает поля из модели в форме.
+        """
         model = Interaction
         fields = ['project', 'channel_of_reference', 'reference_obj', 'company', 'description', 'rating']
 
 
 class UserModelForm(forms.ModelForm):
+    """
+    Класс по созданию формы для модели User.
+    """
 
     class Meta:
+        """
+        Отобраает поля из модели в форме.
+        """
         model = User
         fields = ['photo', 'username', 'first_name', 'last_name', 'email', 'phone_number']
 
